06d106169e0a018df0ad017c5fcf8cf5ee6dc6ab
surachat12345 <51730656+surachat12345@users.noreply.github.com>
2019-10-02 11:28:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def callback():
     return '',200
     
         
-  def sendText(user, text):
+ def sendText(user, text):
   LINE_API = 'https://api.line.me/v2/bot/message/reply'
   headers = {
     'Content-Type': 'application/json; charset=UTF-8',
